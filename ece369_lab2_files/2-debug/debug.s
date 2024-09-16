d45f77d89d9c82bf1aa54675d4808c3ab6a7e042
CarsonKeegan <carsonkeegan04@gmail.com>
2024-09-16 14:59:08 -0700

    2 done

--- ece369_lab2_files/2-debug/debug.s
+++ ece369_lab2_files/2-debug/debug.s
@@ -1,7 +1,8 @@
 # Exercise 2
 #  Max Score: 9 points
 #
-# Students: Giuseppe Pongelupe Giacoia, Carson Keegan, Leo Dickinson 
+# Students: Carson Keegan, Giuseppe Pongelupe, Leo Dickinson
+# Percent Effort: Carson Keegan - 33%, Giuseppe Pongelupe - 33%, Leo Dickinson - 33%
 #
 # 'count_occurence.a' - count the occurrences of a specific character in string 
 # "str". Indexed addressing is used to access the array elements.
@@ -12,8 +13,11 @@
 #
 # Questions:-
 # 1. Briefly describe the purposes of the registers, $t0, $t1, $t2, and $t3.
+#	$t0 holds onto the current char within the string as the program loops through the string values. $t1 continually increments allowing for $t0 to fetch the next char in the string.
+#	$t2 is incremented when a matching character is found and holds the count value. $t3 is initialized with the character that we are trying to count in the string and is used for comparison with $t0.
 # 2. Currently, the program is stuck in an infinite loop. Make use of 
 #    breakpoints to locate, and correct the error.
+#		The error has been fixed, $t1 was not incrementing properly leading to the infinite loop.
 
 	.text
 
@@ -30,11 +34,11 @@ loop:
     lb      $t0, str($t1)	# fetch a character in 'str'
 	beqz    $t0, strEnd	    # if a null character is fetched, exit the loop
 	bne     $t0, $t3, con   # branches to 'con' if registers $t0, and $t3 are not the same
-	add     $t2, $t2, 1	    # increment register $t2
+	addi     $t2, $t2, 1	    # increment register $t2
 
 con:	
 
-    add     $t1, $t2, 1	    # increase indexing register $t1
+    addi     $t1, $t1, 1	    # increase indexing register $t1, Fixed $t1 incrementing
 	j       loop	       	# continues the loop
 
 strEnd:
