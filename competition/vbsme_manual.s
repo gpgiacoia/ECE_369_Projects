9e08beebb7a498f4d1e2001743396ed3fcca3912
fishmaniac <0xffishmaniac@gmail.com>
2024-12-08 19:59:18 -0700

    linear vbsme

--- competition/vbsme_manual.s
+++ competition/vbsme_manual.s
@@ -1,42 +1,72 @@
 .text
 main:
+        la $2, asize1
         la $3, frame1
         la $4, window1
 
-        lw $5, 0(asize1)        // frame_y
-        lw $6, 4(asize1)        // frame_x
-        lw $7, 8(asize1)        // window_y
-        lw $8, 12(asize1)      // window_x
+        lw $5, 0($2)         # frame_y
+        lw $6, 4($2)         # frame_x
+        lw $7, 8($2)         # window_y
+        lw $8, 12($2)        # window_x
 
-        sub $9, $5, $7          // frame_y - window_y
-        sub $10, $6, $8        // frame_x - window_x
+        sub $9, $5, $7           # frame_y - window_y
+        sub $10, $6, $8          # frame_x - window_x
 
-        add $11, $zero, $zero    // y
-        add $12, $zero, $zero    // x
-        add $13, $zero, $zero    // i
-        add $14, $zero, $zero    // j
+        add $11, $zero, $zero    # y
+        add $12, $zero, $zero    # x
+        add $13, $zero, $zero    # i
+        add $14, $zero, $zero    # j
+
+        addi $23, $zero, 16000    # min_sad
+        add $24, $zero, $zero    # min_y
+        add $25, $zero, $zero    # min_x
 
         jal vbsme
+        j loop
+
 sad:
 loop_i:
-        addi $13, $13, 1        // i++
 loop_j:
-        // sad += abs
-        addi $14, $14, 1        // j++
-        bne $14, $8             // if j != window_x
-        bne $13, $7             // if i != window_y
+        add $15, $11, $13       # temp0 = y + i
+        add $16, $12, $14       # temp1 = x + j
+        mul $17, $15, $6        # temp2 = y * frame_x
+        add $18, $17, $16       # frame_index = temp2 + temp1
+        lw $15, 0($18)          # temp0 = frame[frame_index]
+
+        mul $16, $13, $8        # temp1 = i * window_x
+        add $18, $16, $14       # window_index = temp1 + j
+        lw $17, 0($18)          # temp2 = window[window_index]
+
+        sub $19, $15, $17       # SAD = temp0 - temp2
+        bgez $19, positive      # if (SAD >= 0) j positive
+        addi $15, $zero, -1     # temp0 = -1
+        mul $19, $19, $15       # SAD *= -1
+positive:
+        addi $14, $14, 1        # j++
+        bne $14, $8, loop_j     # if j != window_x
+        addi $13, $13, 1        # i++
+        bne $13, $7, loop_i     # if i != window_y
         jr $ra
 
 vbsme:
 loop_y:
-        addi $11, $11, 1        // y++
+        addi $11, $11, 1        # y++
 loop_x:
         jal sad
+        slt $15, $19, $23       # temp0 = SAD < min_sad
+        beq $15, $zero, not_min # if (temp0 == 0) j not_min
+        add $23, $zero, $19     # min_sad = SAD
+        add $24, $zero, $11    # min_y = y
+        add $25, $zero, $12    # min_x = x
 
-        addi $12, $12, 1        // x++
-        bne $12, $10, loop_x    // if x != frame_x - window_x
-        bne $11, $9, loop_y     // if y != frame_y - window_y
+not_min:
+        addi $12, $12, 1        # x++
+        bne $12, $10, loop_x    # if x != frame_x - window_x
+        bne $11, $9, loop_y     # if y != frame_y - window_y
         jr $ra
+
+loop:
+        j loop
 
 .data
 asize1:  .word    16, 16, 8, 4    #i, j, k, l
---
+++ ece369_lab5_files/WBCLK.v
@@ -0,0 +1,68 @@
+`timescale 1ns / 1ps
+
+module WBCLK(
+    input wire Clk, 
+    input wire Reset,
+    
+    // Data inputs
+    input wire [31:0] MemReadData,
+    input wire [31:0] ALUResultIn,
+    input wire [4:0]  RegAddressIn,
+    input wire [31:0] PCIn, 
+    
+    // Control inputs
+    input wire RegWrite,             
+    input wire MemToReg,             
+    input wire [1:0]LoadData, 
+    input wire JrAddress,            
+    input wire JrData,
+            
+    input wire [31:0]PCAdderResultIn,
+    output reg [31:0]PCAdderResultOut,
+    
+    // Data outputs
+    output reg [31:0] MemReadDataOut,
+    output reg [31:0] ALUResultOut,
+    output reg [4:0]  RegAddressOut,
+    output reg [31:0] PCOut,
+    
+    // Control outputs
+    output reg RegWriteOut,             
+    output reg MemToRegOut,             
+    output reg [1:0] LoadDataOut,
+    output reg JrAddressOut,            
+    output reg JrDataOut            
+);
+always @(negedge Clk) begin
+	if (Reset) begin
+	    // Reset all outputs to zero
+	    MemReadDataOut <= 0;
+	    ALUResultOut <= 0;
+	    RegAddressOut <= 0;
+	    PCOut<=0;
+	    
+	    PCAdderResultOut<=0;
+	    // Reset control signals
+	    RegWriteOut <= 0;
+	    MemToRegOut <= 0;
+	    LoadDataOut <= 0;
+	    JrAddressOut <= 0;
+	    JrDataOut <= 0;
+	end else begin
+	    // Pass data inputs to outputs
+	    MemReadDataOut <= MemReadData;
+	    ALUResultOut <= ALUResultIn;
+	    RegAddressOut <= RegAddressIn;
+	    PCOut<=PCIn;
+	    PCAdderResultOut<=PCAdderResultIn;
+
+	    // Pass control signals to outputs
+	    RegWriteOut <= RegWrite;
+	    MemToRegOut <= MemToReg;
+	    LoadDataOut <= LoadData;
+	    JrDataOut<=JrData;
+	    JrAddressOut<=JrAddress;
+	end
+end
+
+endmodule
